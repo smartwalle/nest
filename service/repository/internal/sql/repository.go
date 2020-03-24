d7ad90f788bc0dca42b9cb5c07697cf8dd378be5
smartwalle <smartwalle@gmail.com>
2020-03-24 11:17:17 +0800

    结构调整

--- service/repository/internal/sql/repository.go
+++ service/repository/internal/sql/repository.go
@@ -10,24 +10,31 @@ const Delete nest.Status = -1 // 删除
 
 type Repository struct {
 	db          dbs.DB
-	table       string
 	dialect     dbs.Dialect
 	idGenerator dbs.IdGenerator
+	table       string
 }
 
 func NewRepository(db dbs.DB, dialect dbs.Dialect, table string) *Repository {
 	var r = &Repository{}
 	r.db = db
+	r.idGenerator = dbs.GetIdGenerator()
+	r.dialect = dialect
 
 	table = strings.TrimSpace(table)
 	if table == "" {
 		table = "nest"
 	}
-
 	r.table = table
-	r.idGenerator = dbs.GetIdGenerator()
-	r.dialect = dialect
 	return r
+}
+
+func (this *Repository) DB() dbs.DB {
+	return this.db
+}
+
+func (this *Repository) Dialect() dbs.Dialect {
+	return this.dialect
 }
 
 func (this *Repository) BeginTx() (dbs.TX, *Repository) {
@@ -47,8 +54,8 @@ func (this *Repository) UseIdGenerator(g dbs.IdGenerator) {
 	this.idGenerator = g
 }
 
-func (this *Repository) DB() dbs.DB {
-	return this.db
+func (this *Repository) IdGenerator() dbs.IdGenerator {
+	return this.idGenerator
 }
 
 func (this *Repository) Table() string {
